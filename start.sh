ee7ff71303f67881425b761a55d844763713fa38
Jason Xie <jason.xie@victheme.com>
2022-09-21 02:37:54 +0800

    Added temporary bash script to ensure config.json is at the same folder when user executing the script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Hackish way to force to enter the directory where the config.json
+## Located at. Remove this when go parts can detect config.json smartly
+cd /home/{username}/jxcryptnotify
+
+## Fire the main executable
+./main
